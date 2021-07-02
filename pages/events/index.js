86b4365ae9f7b5d70e88af07f5485f8769f4b014
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-07-02 17:24:35 +0300

    Working on the "All Events" Page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,19 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+
 function AllEventsPage() {
+
+    const events = getAllEvents();
+
     return (
-        <div>
-            <h1>All Events</h1>
-        </div>
-    )
+        <Fragment>
+
+            <EventList items={events} />
+        </Fragment>
+    );
 }
 
 export default AllEventsPage;
