82efcdefa9fea5dce64a2cb7795bebb825492401
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-07-02 23:14:19 +0300

    Adding a Filter Form for Filtering Events

--- pages/events/index.js
+++ pages/events/index.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
 
 function AllEventsPage() {
 
@@ -10,7 +11,7 @@ function AllEventsPage() {
 
     return (
         <Fragment>
-
+            <EventsSearch />
             <EventList items={events} />
         </Fragment>
     );
