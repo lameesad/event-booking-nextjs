dc4892ed75764b5987af4756b421d2ec6082cc63
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-07-05 21:16:16 +0300

    Loading Data & Paths For Dynamic Pages

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,17 +1,14 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
 
-import { getEventById } from '../../dummy-data';
+
+import { getEventById, getAllEvents } from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
-function EventDetailPage() {
+function EventDetailPage(props) {
 
-    const router = useRouter()
-
-    const eventId = router.query.eventId;
-    const event = getEventById(eventId);
+    const event = props.selectedEvents;
 
     if (!event) {
         return (
@@ -36,6 +33,28 @@ function EventDetailPage() {
             </EventContent>
         </Fragment>
     )
+}
+// we need the context because we need to know which specific id we wanna load the data
+export async function getStaticProps(context) {
+    const eventId = context.params.eventId;
+
+    const event = await getEventById(eventId)
+    return {
+        props: {
+            selectedEvents: event
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    const events = await getAllEvents();
+
+    const paths = events.map(event => ({ params: { eventId: event.id } }))
+    return {
+        paths: paths,
+        // we set fallback false because we specify all possible paths
+        fallback: false
+    }
 }
 
 export default EventDetailPage;
