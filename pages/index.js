670940790af87f162080cd3b5afa692a9b5294f5
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-07-07 09:58:00 +0300

    head content

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { getFeaturedEvents } from '../helpers/api-util'
 import EventList from '../components/events/event-list';
 
@@ -6,6 +7,10 @@ function HomePage(props) {
 
   return (
     <div>
+      <Head>
+        <title>NextJs Events</title>
+        <meta name="description" content="Find a lot of great events" />
+      </Head>
       <EventList items={props.events} />
     </div>
   )
