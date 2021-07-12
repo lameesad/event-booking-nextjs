851c410c5aa06d3fc2d9e1f63c873ec3578e2c4a
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-07-12 23:14:26 +0300

    Inserting Comments Into The Database-Getting Data From The Database

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,8 +1,12 @@
+import { MongoClient } from 'mongodb';
 
 
 async function handler(req, res) {
     const eventId = req.query.eventId;
-    console.log(eventId)
+
+    const client = await MongoClient.connect(
+        'mongodb+srv://userdb:userdb@cluster0.bp9sj.mongodb.net/events?retryWrites=true&w=majority'
+    );
 
     if (req.method === 'POST') {
         const { email, name, text } = req.body;
@@ -21,24 +25,32 @@ async function handler(req, res) {
 
 
         const newComment = {
-            id: new Date().toISOString(),
             email,
             name,
-            text
+            text,
+            eventId
         }
-        console.log(newComment);
+
+        const db = client.db();
+
+        const result = await db.collection('comments').insertOne(newComment)
+        console.log(result);
+        newComment.id = result.insertedId
         res.status(201).json({ message: 'added comment', comment: newComment })
     }
 
     if (req.method === 'GET') {
-        const dummyList = [
-            { id: 'c1', name: 'Max', text: 'A first Comment' },
-            { id: 'c2', name: 'Manuel', text: 'A second Comment' },
-        ]
-        res.status(200).json({ comments: dummyList })
+        const db = client.db()
+        const documents = await db
+            .collection('comments')
+            .find()
+            .sort({ _id: -1 })
+            .toArray()
+
+        res.status(200).json({ comments: documents })
     }
 
-
+    client.close()
 }
 
 export default handler;
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -10,12 +10,12 @@ async function handler(req, res) {
         }
 
         const client = await MongoClient.connect(
-            'mongodb+srv://userdb:userdb@cluster0.bp9sj.mongodb.net/newsletter?retryWrites=true&w=majority'
+            'mongodb+srv://userdb:userdb@cluster0.bp9sj.mongodb.net/events?retryWrites=true&w=majority'
         );
-        debugger
+
         const db = client.db();
 
-        await db.collection('emails').insertOne({ email: userEmail });
+        await db.collection('newsletter').insertOne({ email: userEmail });
 
         client.close();
         res.status(201).json({ message: 'Signed up!' });
