67e7a9bc0a2f17c91b87e21f8f6632d95d3dd553
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-07-13 10:49:14 +0300

    Error handling

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { connectDatabase, insertDocument } from '../../helpers/db-util';
 
 async function handler(req, res) {
     if (req.method === 'POST') {
@@ -9,15 +9,24 @@ async function handler(req, res) {
             return;
         }
 
-        const client = await MongoClient.connect(
-            'mongodb+srv://userdb:userdb@cluster0.bp9sj.mongodb.net/events?retryWrites=true&w=majority'
-        );
+        let client;
 
-        const db = client.db();
+        try {
+            client = await connectDatabase();
+        } catch (error) {
+            res.status(500).json({ message: 'Connecting to the database failed!' });
+            return;
+        }
 
-        await db.collection('newsletter').insertOne({ email: userEmail });
+        try {
+            await insertDocument(client, 'newsletter', { email: userEmail });
+            client.close();
+        } catch (error) {
+            res.status(500).json({ message: 'Inserting data failed!' });
+            return;
+        }
 
-        client.close();
+
         res.status(201).json({ message: 'Signed up!' });
     }
 }
